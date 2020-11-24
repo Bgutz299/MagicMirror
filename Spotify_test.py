c74961389d24e8384a6a04edb91d1f3092931960
blgutie1 <blgutie1@github.com>
2020-11-23 17:50:06 -0800

    Adding Test Spotify File

---
+++ Spotify_test.py
@@ -0,0 +1,12 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="ae84dd0be57a443ebf92476bc190490a",
+                                               client_secret="9c6431a32a9c4bf79dd590b912709e8c",
+                                               redirect_uri="http://localhost",
+                                               scope="user-library-read"))
+
+results = sp.current_user_saved_tracks()
+for idx, item in enumerate(results['items']):
+    track = item['track']
+    print(idx, track['artists'][0]['name'], " – ", track['name'])
